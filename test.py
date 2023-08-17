60687104036c252ffa5beccaf2cf858c53677720
абдулла <mr.ad1992@mail.ru>
2023-08-17 07:20:03 +0300

    change code of test

--- test.py
+++
@@ -1,7 +0,0 @@
-number = 10
-while number < 5 :
-    print(f"number = {number}")
-    number += 1
-else :
-    print(f"number = {number}. The work of cycle is done")
-print("The work of programe is done")
