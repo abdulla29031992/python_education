85d27d744a9cee0e7d20678712314dd83a98b7bc
абдулла <mr.ad1992@mail.ru>
2023-08-21 22:16:02 +0300

    change code of task 9

--- урок 2/  задача 9.py
+++ урок 2/  задача 9.py
@@ -4,27 +4,35 @@
 # Input: 5
 # Output: 120
 
-count = 2
-sum = 1
-a = True
+# count = 2
+# sum = 1
+# a = True
 
-while a:
-	try:
-		n = int(input("Введите n: "))
-		if n >= 0:
-			a = False
-		else: print("Введите положительное целое число!")
-	except:
-		print("Введите положительное целое число!")
+# while a:
+# 	try:
+# 		n = int(input("Введите n: "))
+# 		if n >= 0:
+# 			a = False
+# 		else: print("Введите положительное целое число!")
+# 	except:
+# 		print("Введите положительное целое число!")
 
-if n == 0:
-	print("0 ! = 1")
-	exit()
-elif n == 1:
-	print("1 ! = 1")
-	exit()
+# if n == 0:
+# 	print("0 ! = 1")
+# 	exit()
+# elif n == 1:
+# 	print("1 ! = 1")
+# 	exit()
 
-while n >= count and n > 1:
-	sum = sum * count
-	count += 1
-print(n,"! = ",sum)
+def fact(n):
+    i = 1
+    result = 1
+    while i < n:
+        result *= i
+        i += 1
+    return result
+
+
+# n = int(input('Введите число ->'))
+# print(fact(n))
+
