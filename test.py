0bddeaf9d413c26c3070a5f84a0af51299d3bd2a
абдулла <mr.ad1992@mail.ru>
2023-08-21 21:33:56 +0300

    change code of test

--- test.py
+++ test.py
@@ -1,12 +1,23 @@
-sp = [55, 111, True, "sssss", [55,77,88] ]
+# sp = [55, 111, True, "sssss", [55,77,88] ]
 
-for i in range(len(sp)):
-    print(sp[i], end = " ")
+# for i in range(len(sp)):
+#     print(sp[i], end = " ")
 
-print(end = "\n")
-for el in sp:
-    print(el, end = " ")
+# print(end = "\n")
+# for el in sp:
+#     print(el, end = " ")
 
-print(end = "\n")
-for item in enumerate(sp):
-    print(item)
+# print(end = "\n")
+# for item in enumerate(sp):
+#     print(item)
+
+from random import randint
+import time
+
+def show_test(upper: int) -> str :
+    print(randint(1,upper))
+    print(time.time() )
+    return 1234
+
+
+print(show_test(100))
