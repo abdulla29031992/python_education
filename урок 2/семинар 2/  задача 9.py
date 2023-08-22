d689733a1abadf7c4bc2ad1b98664a73e70054ff
абдулла <mr.ad1992@mail.ru>
2023-08-22 13:44:46 +0300

    change code of test

--- урок 2/семинар 2/  задача 9.py
+++ урок 2/семинар 2/  задача 9.py
@@ -4,25 +4,6 @@
 # Input: 5
 # Output: 120
 
-# count = 2
-# sum = 1
-# a = True
-
-# while a:
-# 	try:
-# 		n = int(input("Введите n: "))
-# 		if n >= 0:
-# 			a = False
-# 		else: print("Введите положительное целое число!")
-# 	except:
-# 		print("Введите положительное целое число!")
-
-# if n == 0:
-# 	print("0 ! = 1")
-# 	exit()
-# elif n == 1:
-# 	print("1 ! = 1")
-# 	exit()
 
 def fact(n):
     i = 1
