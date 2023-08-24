5213a296d3e0ce7bf38b0a7beca4b65d2a769a99
абдулла <mr.ad1992@mail.ru>
2023-08-24 22:12:15 +0300

    add code in task 19

--- урок 3/задача 19.py
+++ урок 3/задача 19.py
@@ -1,2 +1,22 @@
 # Задача 19. Дана последовательность из N целых чисел и число K. Необходимо сдвинуть всю последовательность (сдвиг - циклический) на K элементов вправо, K – положительное число.
 # Input: [1, 2, 3, 4, 5] k = 3 Output: [4, 5, 1, 2, 3]
+
+from random import randint
+
+list = [randint(-10,10) for _ in range(10)]
+l=len(list)
+k=3
+print(list)
+result=[]
+
+for i in range(l):
+    index=i-k
+    if index>=l:
+        index=index-l
+    #print(index)
+    result.append(list[index])
+print(result)
+
+for i in range(k):
+     list.insert(0,list.pop())
+print(list)
