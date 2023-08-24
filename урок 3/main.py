711e11ce4cfd0ee869b2716ffaf5e0dd08f4bdb6
абдулла <mr.ad1992@mail.ru>
2023-08-24 21:36:45 +0300

    add main.py

---
+++ урок 3/main.py
@@ -0,0 +1,46 @@
+sp = [55, 44 ,"hello", True, 99.88,100]
+
+# for i in range(len(sp)):
+#     print(sp[i])
+
+# for item in sp:
+#     print(item )
+
+# for i, value in enumerate(sp):
+#     print(i,value)
+
+# print("hello" in sp)
+
+# sp.append("last")
+# sp.insert(0,"zero")
+# # sp.extend([1,2,3])
+# # sp += [1,2,3]*3
+# # print([None]*10)
+# del sp[0]
+# sp.remove(True)
+# a = sp.pop()
+# print(a)
+# print(sp)
+# print(len(sp))
+# list tuple set dict
+# t = (5,7,"privet")
+# print(type(t))
+# print(t[0])
+# s = {8,8,8,8,8,8,8,8}
+# s.add(777)
+# s.discard(77777) 
+# print(s)
+
+# d = {123: 4654454, "тетя ира": [45454,5555]}
+# d["дядя ваня"] = "546545"
+# d[input("Введите имя контакта ")] = input("Введите номер телефона ")
+# d["тетя ира"].append(56465)
+# print(d)
+# del d[123]
+# print(d)
+# print(d.keys())
+# print(d.values())
+# for k in d:
+#     print(k)
+# for key, value in d.items():
+#     print(f"{key} - {value}")
