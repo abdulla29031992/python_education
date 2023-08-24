d6002feb45f6f25887f3934c68f06ca7d1b648f5
абдулла <mr.ad1992@mail.ru>
2023-08-24 13:07:03 +0300

    change code of test

--- урок 2/домашнее задание 2/задача 14.py
+++ урок 2/домашнее задание 2/задача 14.py
@@ -1,16 +1,14 @@
 # Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
 # 10 -> 1 2 4 8
 
-N = int(input('Введите число N '))
+n =  int(input('enter the number: '))
 stop = 0
-P = 2
-for i in range(N):
-    if stop != 1:
-        P = P ** i
-        if P <= N:
-            print(P, end=' ')
-            P = 2
-        else:
-            stop = 1
-    else:
-        i = N
+p = 2
+for i in range(n) :
+    if stop != 1 :
+        p = p ** i
+        if p <= n :
+            print(p, end = " ")
+            p = 2
+            
+    
