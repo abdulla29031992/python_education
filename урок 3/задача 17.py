840ef2b25e65172133ab8a7b4213d144fac23c4b
абдулла <mr.ad1992@mail.ru>
2023-08-24 22:08:32 +0300

    change  code in task 17

--- урок 3/задача 17.py
+++ урок 3/задача 17.py
@@ -4,6 +4,22 @@
 # print(len(set(input().split())))    
 # print(len(set(input())) - 1)  
 
-sp = [1, 1, 2, 0, -1, 3, 4, 4]
-sp = set (sp)
-print(len(sp))
+# sp = [1, 1, 2, 0, -1, 3, 4, 4]
+# sp = set (sp)
+# print(len(sp))
+
+
+from random import randint
+
+
+list = [randint(-10,10) for _ in range(10)]
+
+#for i in range(10):
+#    list.append(randint(-10,10))
+
+print(list)
+
+result = set(list)
+
+print(len(result))
+
