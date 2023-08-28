c8902ec4791eddbf56aede843a9ac1cd943cddb4
абдулла <mr.ad1992@mail.ru>
2023-08-28 19:14:25 +0300

    change code in task 16

--- урок 3/ домашнее задание 3/задача 16.py
+++ урок 3/ домашнее задание 3/задача 16.py
@@ -29,10 +29,10 @@
 
 
 # Ваиант 4
-list_1 = [1, 2, 3, 4, 5]
-k = 3
-count = 0
-for i in range(len(list_1)):
-    if list_1[i] == k:
-        count += 1
-print(count)
+# list_1 = [1, 2, 3, 4, 5]
+# k = 3
+# count = 0
+# for i in range(len(list_1)):
+#     if list_1[i] == k:
+#         count += 1
+# print(count)
