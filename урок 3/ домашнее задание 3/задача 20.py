ac9a81300680ebb64b606681f914dce090435e5a
абдулла <mr.ad1992@mail.ru>
2023-08-28 19:52:57 +0300

    add second and third  code in task 20

--- урок 3/ домашнее задание 3/задача 20.py
+++ урок 3/ домашнее задание 3/задача 20.py
@@ -22,21 +22,72 @@
 # Вывод:
 # 12
 
-points_en = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP',
-             4: 'FHVMY', 5: 'K', 8: 'JX', 10: 'QZ'}
-points_ru = {1: 'АВЕИНОРСТ', 2: 'ДКЛМПУ', 3: 'БГЁЬЯ',
-             4: 'ЙЫ', 5: 'ЖЗХЦЧ', 8: 'ШЭЮ', 10: 'ФЩЪ'}
-alphabet_en = 'QWERTYUIOPASDFGHJKLZXCVBNM'
-word = input().upper()
-point = 0
-if word[0] in alphabet_en:
-    for sym in word:
-        for i in points_en:
-            if sym in points_en[i]:
-                point += i
-else:
-    for sym in word:
-        for i in points_ru:
-            if sym in points_ru[i]:
-                point += i
-print(point)
+
+# variant 1
+
+# points_en = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP',
+#              4: 'FHVMY', 5: 'K', 8: 'JX', 10: 'QZ'}
+# points_ru = {1: 'АВЕИНОРСТ', 2: 'ДКЛМПУ', 3: 'БГЁЬЯ',
+#              4: 'ЙЫ', 5: 'ЖЗХЦЧ', 8: 'ШЭЮ', 10: 'ФЩЪ'}
+# alphabet_en = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+# word = input().upper()
+# point = 0
+# if word[0] in alphabet_en:
+#     for sym in word:
+#         for i in points_en:
+#             if sym in points_en[i]:
+#                 point += i
+# else:
+#     for sym in word:
+#         for i in points_ru:
+#             if sym in points_ru[i]:
+#                 point += i
+# print(point)
+
+
+# variant 2
+
+# import re
+# def Scrabble(text):
+# 	return bool(re.search("[а-яА-Я]", text))
+# Rus = { 1:"А, В, Е, И, Н, О, Р, С, Т",
+#       	2:"Д, К, Л, М, П, У",
+#         3:"Б, Г, Ё, Ь, Я",
+#         4:"Й, Ы",
+#         5:"Ж, З, Х, Ц, Ч",
+#         8:"Ш, Э, Ю",
+#         10:"Ф, Щ, Ъ"}
+# Eng = { 1:"A, E, I, O, U, L, N, S, T, R ",
+#         2:"D, G",
+#         3:"B, C, M, P",
+#         4:"F, H, V, W, Y",
+#         5:"K",
+#         8:"J, X"}
+# text = input("Введите слово: ").upper()
+# if Scrabble(text):
+# 	print(sum([k for i in text for k, v in Rus.items() if i in v]))
+# else:
+# 	print(sum([k for i in text for k, v in Eng.items() if i in v]))
+
+
+# variant 3
+
+# k = 'ноутбук'
+# points_en = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP', 4: 'FHVWY', 5: 'K', 8: 'JX', 10: 'QZ'}
+# points_ru = {1: 'АВЕИНОРСТ', 2: 'ДКЛМПУ', 3: 'БГЁЬЯ', 4: 'ЙЫ', 5: 'ЖЗХЦЧ', 8: 'ШЭЮ', 10: 'ФЩЪ'}
+# word = k.upper()  # переводим все буквы в верхний регистр
+# count = 0
+# for i in word:
+#     if i in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+#         for j in points_en:
+#             if i in points_en[j]:
+#                 count = count + j
+#     else:
+#         for j in points_en:
+#             if i in points_ru[j]:
+#                 count = count + j
+# print(count)
+
+
+
+
