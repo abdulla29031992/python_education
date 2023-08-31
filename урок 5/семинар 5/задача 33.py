b988b7014d2e5c3023a0e836897f53f6f10693f2
абдулла <mr.ad1992@mail.ru>
2023-08-31 22:15:04 +0300

    add code in task 33

--- урок 5/семинар 5/задача 33.py
+++ урок 5/семинар 5/задача 33.py
@@ -2,3 +2,31 @@
 # Напишите программу, которая заменяет оценки Василия, но наоборот: все максимальные – на минимальные.
 # Input: 5 -> 1 3 3 3 4 
 # Output: 1 3 3 3 1
+
+# def max_change_min():
+#     a = [0]
+#     count = int(input('Кол-во оценок: '))
+#     a = a * count
+#     for j in range(len(a)):
+#         num = int(input('Оценка: '))
+#         a[j] = num
+#     print('Исходник:',a)
+#     for i in range(len(a)):
+#         if a[i] > 3:
+#             a[i] = 1
+#     print('Коррекция: ',a)
+
+# max_change_min()
+
+def Change_Array(array, max_array, min_array):
+    for i in range(len(array)):
+        if array[i] == max_array:
+            array[i] = min_array
+    return array
+
+
+
+array = [1, 3, 3, 3, 4]
+max_array = max(array)
+min_array = min(array)
+print(Change_Array(array, max_array, min_array))
