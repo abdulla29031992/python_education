caa978f1f23f88d12ce7b05ccb5d408a0b48c2c4
абдулла <mr.ad1992@mail.ru>
2023-09-04 22:02:52 +0300

    add second  code in task 41

--- урок 6/семинар 6/задача 41.py
+++ урок 6/семинар 6/задача 41.py
@@ -16,6 +16,6 @@ for i in range(-1, len(list) - 1):
         count += 1
 print(count)
 
-
+# запись в одну строку
 # print(sum([1 for x in range(-1, len(list_1)-1) if list_1[x] > list_1[x+1] and list_1[x] > list_1[x-1]]))
 # print(sum([1 for x in range(-1, len(list_1)-1) if list_1[x-1] < list_1[x] > list_1[x+1]]))
