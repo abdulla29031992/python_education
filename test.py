1095825a79d1f785b9238061b3a2c2e0a498a42b
абдулла <mr.ad1992@mail.ru>
2023-09-04 19:37:15 +0300

    created folder lesson 6,add task 39

---
+++ test.py
@@ -0,0 +1,17 @@
+# import random
+
+# n = int(input())
+# lst1=[random.randint(0,10) for i in range(n)]
+# print(lst1)
+
+# m = int(input())
+# lst2=[random.randint(0,10) for i in range(m)]
+# print(lst2)
+
+# lst3=[]
+# for i in lst1:
+#     if i not in lst2:
+#         lst3.append(i)
+
+# print(lst3) 
+
