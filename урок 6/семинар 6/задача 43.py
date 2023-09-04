1332c0f29cb74f65de7fe65dbeeed4b61cb71368
абдулла <mr.ad1992@mail.ru>
2023-09-04 22:22:15 +0300

    add code in task 43

--- урок 6/семинар 6/задача 43.py
+++ урок 6/семинар 6/задача 43.py
@@ -2,3 +2,17 @@
 # Считается, что любые два элемента, равные друг другу образуют одну пару, которую необходимо посчитать. 
 # Вводится список чисел. Все числа списка находятся на разных строках.
 
+import random
+
+n = int(input())
+list = [random.randint(0, 10) for i in range(n)]
+print(list)
+
+count = 0
+
+for i in range(len(list)):
+    for j in range(i + 1, len(list)):
+        if list[i] == list[j]:
+            count += 1
+
+print(count)
