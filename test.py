12bccf02a6c90c61122fa8c247a04dddaa7d7a8c
абдулла <mr.ad1992@mail.ru>
2023-09-04 19:52:47 +0300

    add task 41

--- test.py
+++ test.py
@@ -15,3 +15,18 @@
 
 # print(lst3) 
 
+
+
+# import random
+
+# n = int(input())
+# lst = [random.randint(0, 10) for i in range(n)]
+# print(lst)
+
+# count = 0
+# for i in range(1, len(lst)-1):
+#     if lst[i] > lst[i-1] and lst[i] > lst[i+1]:
+#         count += 1
+# print(count)
+
+
