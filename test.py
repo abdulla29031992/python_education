12f8d60926a8b39b3104dca986c80568e0cdd58f
абдулла <mr.ad1992@mail.ru>
2023-09-04 20:24:27 +0300

    add task 43, 45

--- test.py
+++ test.py
@@ -17,6 +17,7 @@
 
 
 
+
 # import random
 
 # n = int(input())
@@ -30,3 +31,33 @@
 # print(count)
 
 
+
+# import random
+
+# n = int(input())
+# list = [random.randint(0, 10) for i in range(n)]
+# print(list)
+
+# count = 0
+
+# for i in range(len(list)):
+#     for j in range(i+1,len(list)):
+#         if list[i] == list[j]:
+#             count += 1
+# print(count)
+
+
+# k = int(input())
+
+# if k < pow(10, 5):
+#     for n in range(1, k):
+#         m = 0
+#         sum2 = 0
+#         for i in range(1, n//2+1):
+#             if n % i == 0:
+#                 m += i
+#         for j in range(1, m//2+1):
+#             if m % j == 0:
+#                 sum2 += j
+#         if sum2 == n and m > n:
+#             print(n, m)
