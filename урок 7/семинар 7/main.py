97f9e5cb7b4c35012f90b82da59bead70b61a713
абдулла <mr.ad1992@mail.ru>
2023-09-10 14:37:01 +0300

    add task 47

--- урок 7/семинар 7/main.py
+++ урок 7/семинар 7/main.py
@@ -1,42 +1,61 @@
-from time import time
+# from time import time
 
-def how_long(func):
-    start = time()
-    func()
-    print(f"на это ушло {time() - start} секунд")
+# def how_long(func):
+#     start = time()
+#     func()
+#     print(f"на это ушло {time() - start} секунд")
 
-def create_list():
-    return [i for i in range(5000000)]
+# def create_list():
+#     return [i for i in range(5000000)]
 
 # how_long(create_list)
-def calc_power(pow):
-    def calc_value(root):
-        return root ** pow
-    return calc_value
 
+
+
+# def calc_power(pow):
+#     def calc_value(root):
+#         return root ** pow
+#     return calc_value
 # print(calc_power(2)(3))
-# # print(calc_power(2))
 # square = calc_power(2)
 # cube = calc_power(3)
 # print(square(8))
 # print(cube(2))
 
 
+
+# square_2= lambda x: x** 2
+# print(square_2(7))
+
+
+
+
 # def obrab(x,y,calc):
 #     for key in calc:
 #         print(calc[key](x,y))
+
+# calc = {'+': lambda x,y: x + y,
+#         '-': lambda x,y: x - y,
+#         '*': lambda x,y: x * y,
+#         '/': lambda x,y: x / y }
+
+# obrab(7,4,calc)
 
 
 # calc = {'+': lambda x,y: x + y,
 #         '-': lambda x,y: x - y,
 #         '*': lambda x,y: x * y,
 #         '/': lambda x,y: x / y }
-# # print(calc['*'](7,4))
-# # print(calc['/'](7,4))
-# obrab(7,4,calc)
 
-sp = [7, 8, 11, -5, -9, -7]
-sp2 = ["Иван", "Коля"]
+# print(calc['*'](7,4))
+# print(calc['/'](7,4))
+
+
+
+
+
+# sp = [7, 8, 11, -5, -9, -7]
+# sp2 = ["Иван", "Коля"]
 
 # print(sp)
 # print(type(*sp))
@@ -47,5 +66,5 @@ sp2 = ["Иван", "Коля"]
 
 # print(*filter(lambda item: item > 0,  sp))
 
-for item in zip(sp2, sp):
-    print(item)
+# for item in zip(sp2, sp):
+#     print(item)
