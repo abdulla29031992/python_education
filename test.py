8c6a986ec34cd0d0b59608d9e71e9b0b297591ab
абдулла <mr.ad1992@mail.ru>
2023-09-11 06:25:00 +0300

    change code in task of 36

--- test.py
+++ test.py
@@ -1,3 +1,35 @@
+# def print_operation_table(operation, n, m):
+#     table = []
+#     for i in range(1, n + 1):
+#         line = []
+#         for j in range(1, m + 1):
+#             line.append(operation(i,j))
+#         table.append(line)
+
+#     for row in table:
+#         print(("{: <7}"*m).format(*row))
+
+#     print("Искомый элемент> ", operation(n,m))
+
+# x, y = map(int, input("Введите 2 значения через пробел: ").split())
+# print_operation_table(lambda x, y: x * y, x, y)
+
+
+
+# def print_operation_table(operation, num_rows=6, num_columns=6):
+#     a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+#     for i in a:
+#         print(*[f"{x:>3}" for x in i])
+
+# print_operation_table(lambda x, y: x * y)
+
+def print_operation_table(operation, num_rows = 6, num_colums = 6):
+    a = [[operation(i, j) for j in range(1, num_colums + 1)] for i in range(1, num_rows + 1)]
+    for i in a:
+        print(*[f"{x:>3}" for x in i])
+
+print_operation_table(lambda x, y: x * y)
+
 
 
 
--- урок 7/домашняя работа/задача 36.py
+++ урок 7/домашняя работа/задача 36.py
@@ -14,18 +14,9 @@
 # 5 10 15 20 25 30
 # 6 12 18 24 30 36
 
-def print_operation_table(operation, n, m):
-    table = []
-    for i in range(1, n + 1):
-        line = []
-        for j in range(1, m + 1):
-            line.append(operation(i,j))
-        table.append(line)
+def print_operation_table(operation, num_rows = 6, num_colums = 6):
+    a = [[operation(i, j) for j in range(1, num_colums + 1)] for i in range(1, num_rows + 1)]
+    for i in a:
+        print(*[f"{x:>3}" for x in i])
 
-    for row in table:
-        print(("{: <7}"*m).format(*row))
-
-    print("Искомый элемент> ", operation(n,m))
-
-x, y = map(int, input("Введите 2 значения через пробел> ").split())
-print_operation_table(lambda x, y: x * y, x, y)
+print_operation_table(lambda x, y: x * y)
